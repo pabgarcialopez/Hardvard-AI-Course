0e409a9681dd2ec01b367ada346b40c84ae369f3
pabgarcialopez <pabgarcialopez@gmail.com>
2025-02-17 13:32:03 +0100

    Completed puzzle.py and added minesweeper to project

--- Week 1/knights/puzzle.py
+++ Week 1/knights/puzzle.py
@@ -1,5 +1,6 @@
 from logic import *
 
+# Symbols for A, B, and C
 AKnight = Symbol("A is a Knight")
 AKnave = Symbol("A is a Knave")
 
@@ -8,6 +9,10 @@ BKnave = Symbol("B is a Knave")
 
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
+
+# A new symbol to capture what A said:
+# ASaidKnave is True if A said "I am a Knave", and False if A said "I am a Knight".
+ASaidKnave = Symbol("A said 'I am a Knave'")
 
 # Puzzle 0
 # A says "I am both a knight and a knave."
@@ -24,25 +29,58 @@ knowledge0 = And(
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    # A and B are both either a Knight ot a Knave
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    # But not both
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+
+    Biconditional(AKnight, And(AKnave, BKnave))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    # A and B are both either a Knight ot a Knave
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    # But not both
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+
+    # They are of the same kind
+    Biconditional(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    # They are of diiferent kinds
+    Biconditional(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight)))
 )
 
-# Puzzle 3
-# A says either "I am a knight." or "I am a knave.", but you don't know which.
-# B says "A said 'I am a knave'."
-# B says "C is a knave."
-# C says "A is a knight."
 knowledge3 = And(
-    # TODO
-)
+    # Each person is either a Knight or a Knave (but not both).
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)),
+    Or(CKnight, CKnave),
+    Not(And(CKnight, CKnave)),
 
+    # A's statement:
+    # If ASaidKnave is true, then A said "I am a Knave" (i.e., his statement is AKnave).
+    # If ASaidKnave is false, then A said "I am a Knight" (i.e., his statement is AKnight).
+    Biconditional(AKnight, Or(And(ASaidKnave, AKnave),
+                                And(Not(ASaidKnave), AKnight))),
+
+    # B says: "A said 'I am a Knave'." 
+    # (If B is a Knight, then it must be true that ASaidKnave is true.)
+    Biconditional(BKnight, ASaidKnave),
+
+    # B says: "C is a Knave."
+    Biconditional(BKnight, CKnave),
+
+    # C says: "A is a Knight."
+    Biconditional(CKnight, AKnight)
+)
 
 def main():
     symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
